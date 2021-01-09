2e19686dabecac3eecc651ecc125470e31c9d011
William Anderson <willjatlas@googlemail.com>
2021-01-09 15:57:04 +0000

    adding file structure to client

--- client/containers/GameDisplay.js
+++ client/containers/GameDisplay.js
@@ -3,6 +3,9 @@ import {useState} from "react";
 const GameDisplay = ()=>{
 
     const [playerScore, setPlayerScore] = useState(0);
-    const []
+    const [turnScore, setTurnScore]     = useState(0);
+    const [playerName, setPlayerName]   = useState("");
+    
+
 
 }
