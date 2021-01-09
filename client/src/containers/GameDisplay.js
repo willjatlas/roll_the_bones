a81cf9a50c6e8cc6a34acef57502f657b9d8320c
William Anderson <willjatlas@googlemail.com>
2021-01-09 18:10:58 +0000

    working on rolling the dice pseudocode

--- client/src/containers/GameDisplay.js
+++ client/src/containers/GameDisplay.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import DiceDisplay from "./DiceDisplay"
 
 const GameDisplay = ()=>{
@@ -6,7 +6,23 @@ const GameDisplay = ()=>{
     const [playerScore, setPlayerScore] = useState(0);
     const [turnScore, setTurnScore]     = useState(0);
     const [playerName, setPlayerName]   = useState("");
-    
+    //Decrease diceToRoll as the roll sticking values
+    const [diceToRoll, setDiceToRoll]   = useState(5);
+    //If the value is 0, do not render the dice as it is inactive
+    const [rollValues, setRollValues]   = useState([0,0,0,0,0]);
+
+    const rollDice = ()=>{
+        //Returns the random dier rolls of available dice.
+    }
+
+    const handleRollButton = ()=>{
+        //Handle the button that rolls the available dice
+    }
+
+    useEffect(()=>{
+
+    }, [])
+
     
 
     return(
