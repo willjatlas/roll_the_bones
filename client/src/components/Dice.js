8e30b7fb6b0df79b93bbc971b8d9c654c6ff06ef
William Anderson <willjatlas@googlemail.com>
2021-01-09 17:58:15 +0000

    dice images displayed in dice display ok

--- client/src/components/Dice.js
+++ client/src/components/Dice.js
@@ -1,9 +1,24 @@
+import dice1 from "../dice_images/Dice1.png"
+import dice2 from "../dice_images/Dice2.png"
+import dice3 from "../dice_images/Dice3.png"
+import dice4 from "../dice_images/Dice4.png"
+import dice5 from "../dice_images/Dice5.png"
+import dice6 from "../dice_images/Dice6.png"
+
 const Dice = ()=>{
 
     return(
+        <>
         <p>this is a die</p>
+        <img class="dice-images" src={dice1} alt="Dice Face Showing 1" />
+        <img class="dice-images" src={dice2} alt="Dice Face Showing 1" />
+        <img class="dice-images" src={dice3} alt="Dice Face Showing 1" />
+        <img class="dice-images" src={dice4} alt="Dice Face Showing 1" />
+        <img class="dice-images" src={dice5} alt="Dice Face Showing 1" />
+        <img class="dice-images" src={dice6} alt="Dice Face Showing 1" />
+        </>
     )
-    
+
 }
 
 export default Dice;
--- client/src/containers/DiceDisplay.js
+++ client/src/containers/DiceDisplay.js
@@ -1,7 +1,11 @@
+import Dice from "../components/Dice"
 const DiceDisplay = ()=>{
 
     return(
+        <>
         <h3>This is the dice container</h3>
+        <Dice />
+        </>
     )
 };
 
