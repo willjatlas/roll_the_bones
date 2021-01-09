2a90d4ab6108e9a66220febeefd2a023c5bec8ae
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-01-09 16:20:38 +0000

    more back-end stuff

---
+++ server/db/seeds.js
@@ -0,0 +1,31 @@
+db.dropDatabase();
+db.scores.insertMany([
+  {
+    playerName: 'Jack Sparrow',
+    score: 30,
+  },
+  {
+    playerName: 'Captain Hook',
+    score: 14,
+  },
+  {
+    playerName: 'Mr Smee',
+    score: 35,
+  },
+  {
+    playerName: 'Davy Jones',
+    score: 56,
+  },
+  {
+    playerName: 'John Silver',
+    score: 60,
+  },
+  {
+    playerName: 'Edward Teach',
+    score: 99,
+  },
+  {
+    playerName: 'Anne Bonny',
+    score: 23,
+  }
+]);
