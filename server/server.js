cf7670f6f3759a16fd85a71a518d67bb52da81f4
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-01-09 14:36:39 +0000

    node server setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function() {
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
 
+
+app.listen(5000, function() {
+    console.log('App is running on port 5000');
 });
