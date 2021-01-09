423431239a5280d1196c8403d32295a8ff89a1a4
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2021-01-09 17:44:03 +0000

    seeds and router api working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const parser = require('body-parser');
+app.use(parser.json());
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-});
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
 
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true })
+    .then((client)=>{
+        const db = client.db('players');
+        const scoresCollection = db.collection('scores');
+        const playersRouter = createRouter(scoresCollection);
+        app.use('/api/players', playersRouter);
+    })
 
 app.listen(5000, function() {
     console.log('App is running on port 5000');
