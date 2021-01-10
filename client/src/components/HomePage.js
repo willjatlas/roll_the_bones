0b91bb2a9107a3c07885dee423263e0f3190aa47
Donald <donaldmacrury88@gmail.com>
2021-01-10 20:36:14 +0000

    added rules to homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -4,6 +4,17 @@ const Home = () => {
     <div>
         <h2>Home</h2>
         <p>Welcome aboard ye scallywags!</p>
+        <p>Avast, up for a game of Roll the Bones?</p>
+        <p>You'll have to be canny!</p>
+        <p>Ye have 5 dice, ye get to roll them all and</p>
+        <p>add up your total for that roll</p>
+        <p>but if ye get a two or a five</p>
+        <p>thats bones for you Matey!!</p>
+        <p>those dice won't rolle anymore</p>
+        <p>ye get to keep rolling with the rest untill</p>
+        <p>ye run out of dice!!</p>
+        <p>your totall gets added up and that'll be your</p>
+        <p>final score Matey, you might even make the Leaderboard!</p>
     </div>
     )
 };
