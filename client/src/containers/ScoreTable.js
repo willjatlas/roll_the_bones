904d97d44d3417d21722a35f2c898c682c3eba72
Donald <donaldmacrury88@gmail.com>
2021-01-10 21:05:08 +0000

    TODO possibly .sort on scores to order by score value

--- client/src/containers/ScoreTable.js
+++ client/src/containers/ScoreTable.js
@@ -1,7 +1,7 @@
 import Score from "../components/Score";
 
 const ScoreTable = ({highScores}) => {
-
+    // TODO: possibly .sort on scores so that they display by score value
     const scores = highScores.map((element) => {
         return <li> NAME: { element.playerName } SCORE: { element.score }</li>
     });
