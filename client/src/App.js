4410258b5bdbcd7394c283b2e4f3aa5c319d059f
Donald <donaldmacrury88@gmail.com>
2021-01-10 14:10:57 +0000

    started route for ScoreTable page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import {BrowerRouter as Router, Route, Switch} from "react-router-dom";
 
 import GameDisplay from "./containers/GameDisplay";
+import ScoreTable from "./containers/ScoreTable";
 
 function App() {
   return (
     <Router>
       <>
         <Route path="/" component={GameDisplay}/>
+        <Route path="/scoreTable" component={ScoreTable}/>
         
       </>
     </Router>
