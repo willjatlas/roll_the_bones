1e19dbd78633a99ec1cc4a9995561e97af325284
Donald <donaldmacrury88@gmail.com>
2021-01-13 17:47:49 +0000

    playerName updated with random additions

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -1,6 +1,10 @@
+import jack5 from '../card_images/jack5.jpeg';
 const ErrorPage = () => {
+  // const jack = jack5;
+
     return(
       <h1>404 - You've found Davie Jones Locker landlubber!!</h1>
+      // <img>{jack}</img>
     )
 
 }
